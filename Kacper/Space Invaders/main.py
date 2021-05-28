1d1e339e057a3a6836654e42bb1796e4785ea945
Kacper-So <66113631+Kacper-So@users.noreply.github.com>
2021-05-28 14:51:45 +0200

    0.0

--- Kacper/Space Invaders/main.py
+++ Kacper/Space Invaders/main.py
@@ -1,4 +1,13 @@
+import pygame as pg
 
 
 if __name__ == '__main__':
-    print("Hello world")
+    window = pg.display.set_mode((480,320))
+
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                sys.exit(0)
+
+        pg.draw.rect(window, (0,150,255), pg.Rect(10,50,200,100))
+        pg.display.flip()

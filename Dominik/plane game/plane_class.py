17a3545eb705e67d0d7eec6564f08d1a62b8f107
Dominik <domin.bogiel@gmail.com>
2021-05-28 21:38:37 +0200

    plane game 2.0

--- Dominik/plane game/plane_class.py
+++ Dominik/plane game/plane_class.py
@@ -8,18 +8,18 @@ class Plane(pygame.Rect):
         #information about screen location
         self._screen=screen
         middle=screen.get_width() / 2*15/22
-        self._blocks.append([middle, screen.get_height() - 20 - 1])
-        self._blocks.append([middle, screen.get_height() - 20 - 1 - 21])
-        self._blocks.append([middle+ 21, screen.get_height() - 20 - 1])
-        self._blocks.append([middle - 21, screen.get_height() - 20 - 1])
+        self._blocks.append([middle, screen.get_height() - 20 - 1-5])
+        self._blocks.append([middle, screen.get_height() - 20 - 1 - 21-5])
+        self._blocks.append([middle+ 21, screen.get_height() - 20 - 1-5])
+        self._blocks.append([middle - 21, screen.get_height() - 20 - 1-5])
 
     def draw(self):
         for block in self._blocks:
-            pygame.draw.rect(self._screen, (19, 56, 207), pygame.Rect(block[0],block[1],20,20))
+            pygame.draw.rect(self._screen, (255,255, 255), pygame.Rect(block[0],block[1],20,20))
 
     def move(self,dx):
         #_blocks[0] - the middle block of plane
-        if self._blocks[0][0]+dx-10>0 and self._blocks[0][0] + dx < 15/22*self._screen.get_width()+20:
+        if self._blocks[0][0]+dx>22 and self._blocks[0][0] + dx < 14/22*self._screen.get_width()+20:
             for block in self._blocks:
                 block[0]+=dx
 

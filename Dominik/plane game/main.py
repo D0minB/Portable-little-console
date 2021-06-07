df680543162463b5184b07a39715c1f12fbfd6d6
Dominik <domin.bogiel@gmail.com>
2021-06-07 21:37:51 +0200

    Wzrost trudności, poprawa kolizji, pozycje jako Vector2

--- Dominik/plane game/main.py
+++ Dominik/plane game/main.py
@@ -2,33 +2,34 @@
 # AUTHOR: DOMINIK BOGIELCZYK
 
 import pygame
-import sys
+from pygame.math import Vector2
 import time
+import sys
 import random
-import numpy as np
+import RPi.GPIO as GPIO
 from plane_class import Plane
+from MCP3008_class import MCP3008
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(37,GPIO.IN,pull_up_down=GPIO.PUD_UP)
 
 pygame.init()
-#screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-screen = pygame.display.set_mode((480, 320))
+screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 pygame.display.set_caption('Portable little console')
+pygame.mouse.set_visible(0)
 
-font_style = pygame.font.SysFont("bahnschrift", 40)
-font_small = pygame.font.SysFont("bahnschrift", 15)
+font70 = pygame.font.SysFont("bahnschrift", 70)
+font40 = pygame.font.SysFont("bahnschrift", 40)
+font20 = pygame.font.SysFont("bahnschrift", 20)
+
+def write_text(font,text,color,pos):
+    text = font.render(text, True, color)
+    screen.blit(text, text.get_rect(center=(pos.x, pos.y)))
+
 
 def plane_game():
-    dt = 0.0
-    fps = 20.0
-    clock = pygame.time.Clock()
-
-    plane = Plane(screen)
-    targets = []
-    bullets=[]
-
-    points=0
-
-
-    # NEW TARGETS GENERATING
+# NEW TARGETS GENERATING
     def targets_generating():
         new_targets_pos = []
         for i in range(random.randint(4, 7)):  # 6 TO 12 - NUMBER OF THE NEW TARGETS
@@ -40,13 +41,26 @@ def plane_game():
             new_targets_pos.append(n)
 
         for target_pos in new_targets_pos:  # ADD UNIQUE TARGETS POSITIONS TO LIST
-            targets.append([target_pos * 22 + 8, 8])
+            targets.append(Vector2(target_pos * 22 + 8, 8))
 
         return targets
+        
+    dt = 0.0
+    fps = 20.0
+    clock = pygame.time.Clock()
+
+    adc = MCP3008()
+
+    plane = Plane(screen)
+    targets = []
+    bullets=[]
+
+    points=0
+    
+    n=0
 
     last_targets_time = time.time()
-    last_bullet_time=time.time()
-    last_bullets_move=time.time()
+    new_targets_time=2.5
 
     # GENERATE FIRST TARGETS
     targets_generating()
@@ -58,42 +72,40 @@ def plane_game():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-
-
+        
         while dt > 1 / fps:
             dt -= 1 / fps
-
+            
             # plane move
             delta_x = 0
-
             # plane controlling
-            if pygame.key.get_pressed()[pygame.K_LEFT]:
-                delta_x -= int(150/fps)
-            if pygame.key.get_pressed()[pygame.K_RIGHT]:
-                delta_x += int(150/fps)
+            if pygame.key.get_pressed()[pygame.K_LEFT] or adc.read(channel=2)<=50:
+                delta_x -= 200/fps
+            if pygame.key.get_pressed()[pygame.K_RIGHT] or adc.read(channel=2)>=700:
+                delta_x += 200/fps
             plane.move(delta_x)
 
-            # bullets generating
-            if time.time() - last_bullet_time > 0.1:
-                last_bullet_time = time.time()
+            if n>=2:
+                n=0
+            else:
+                n+=1
 
-                if pygame.key.get_pressed()[pygame.K_SPACE]:
-                    bullets.append([plane.get_position()[0] + 10, plane.get_position()[1] - 10])
+            #bullet every 3 frames
+            if n==2 and (GPIO.input(37) == 0 or pygame.key.get_pressed()[pygame.K_SPACE]):
+                bullets.append(Vector2(plane.get_position()[0] + 10, plane.get_position()[1] - 10))
 
-            if time.time() - last_bullets_move > 0.02:
-                last_bullets_move = time.time()
-
-                #bullets move in y-axis
-                for bullet in bullets:
-                    bullet[1]-=150/fps
+            #bullets move in y-axis
+            for bullet in bullets:
+                bullet.y-=150/fps
 
             # new targets every 3 seconds
-            if time.time() - last_targets_time > 3.0:
+            if time.time() - last_targets_time > new_targets_time:
                 last_targets_time = time.time()
+                new_targets_time=0.95*new_targets_time
 
                 # previous targets move in y-axis
                 for target in targets:
-                    target[1] += 22
+                    target.y += 22
 
                 # generate new targets
                 targets_generating()
@@ -101,38 +113,20 @@ def plane_game():
             targets_copy=targets
             bullets_copy=bullets
 
+            #collision - target and bullet
             for target in targets_copy:
                 for bullet in bullets_copy:
-                    if pygame.Rect(target[0],target[1],20,20).colliderect(pygame.Rect(bullet[0]-10,bullet[1]-10,10,10)):
+                    if pygame.Rect(target.x,target.y,20,20).colliderect(pygame.Rect(bullet.x-5,bullet.y-5,10,10)):
                         targets.remove(target)
                         bullets.remove(bullet)
                         points+=1
 
             #remove out of screen bullets
             for bullet in bullets_copy:
-                if bullet[1]<15:
+                if bullet.x<15:
                     bullets.remove(bullet)
 
-            [x,y]=plane.get_position()
-
-            if len(targets)>0 and targets[0][1] > y:
-                screen.fill((0, 0, 0))
-                text_end = font_style.render("YOU LOST", True, (255, 0, 0))
-                screen.blit(text_end, [480/2 - 75, 256/2 - 100])
-                text = font_style.render(str(points), True, (255, 0, 0))
-                screen.blit(text, [screen.get_width() / 2 - 25, screen.get_height() - 150])
-                text = font_small.render("press q to quit or space to new game", True, (255, 255, 255))
-                screen.blit(text, [120, 220])
-
-                pygame.display.update()
-
-                while True:
-                    for event in pygame.event.get():
-                        if event.type == pygame.QUIT or pygame.key.get_pressed()[pygame.K_q]:
-                            pygame.quit()
-                            sys.exit()
-                        if pygame.key.get_pressed()[pygame.K_SPACE]:
-                            plane_game()
+            
 
             # cleaning screen
             screen.fill((0, 0, 0))
@@ -146,15 +140,31 @@ def plane_game():
             pygame.draw.line(screen,(255,255,255),(16*22 + 8,2),(16*22 + 8,screen.get_height()-2),4)
 
             for target in targets:
-                pygame.draw.rect(screen, (255, 255, 0),pygame.Rect(target[0],target[1],20,20))
+                pygame.draw.rect(screen, (255, 255, 0),pygame.Rect(target.x,target.y,20,20))
 
             for bullet in bullets:
-                pygame.draw.circle(screen, (255, 20, 0),[int(bullet[0]),int(bullet[1])],5)
+                pygame.draw.circle(screen, (255, 20, 0),[int(bullet.x),int(bullet.y)],5)
 
-            text = font_style.render(str(points), True, (255, 255, 255))
-            screen.blit(text, [screen.get_width() -75, screen.get_height()/2])
-            text2 = font_small.render("points", True, (255, 255, 255))
-            screen.blit(text2, [screen.get_width() - 75, screen.get_height() / 2 +50])
+            write_text(font70,str(points),(0, 0, 255),Vector2(screen.get_width() -50, screen.get_height()/2))
+            write_text(font20,"points",(0, 0, 255),Vector2(screen.get_width() - 50, screen.get_height() / 2 +50))
+
+            plane_position=plane.get_position()
+            
+            #end screen
+            if len(targets)>0 and targets[0].y > plane_position.y-15:
+                screen.fill((0, 0, 0))
+                write_text(font40,"Your score:",(255, 0, 0),Vector2(screen.get_width()/2, 75))
+                write_text(font70,str(points),(255, 0, 0),Vector2(screen.get_width()/2, 150))
+                write_text(font20,"Joystick left to quit or right to new game",(255, 255, 255),Vector2(screen.get_width()/2, 220))
+                pygame.display.update()
+
+                while True:
+                    for event in pygame.event.get():
+                        if event.type == pygame.QUIT or adc.read(channel=2)<=50:
+                            pygame.quit()
+                            sys.exit()
+                        if adc.read(channel=2)>=700:
+                            plane_game()
 
             pygame.display.flip()
 
--- Dominik/plane game/plane_class.py
+++ Dominik/plane game/plane_class.py
@@ -1,27 +1,26 @@
 import pygame
+from pygame.math import Vector2
 
 class Plane(pygame.Rect):
 
     def __init__(self,screen):
         #plane consist of blocks
-        self._blocks = []
+        middle=screen.get_width() / 2*15/22
+        self._blocks = (Vector2(middle,screen.get_height()-26), Vector2(middle,screen.get_height()-47), Vector2(middle+ 21, screen.get_height() - 26),Vector2(middle - 21, screen.get_height() - 26))
+        
         #information about screen location
         self._screen=screen
         middle=screen.get_width() / 2*15/22
-        self._blocks.append([middle, screen.get_height() - 20 - 1-5])
-        self._blocks.append([middle, screen.get_height() - 20 - 1 - 21-5])
-        self._blocks.append([middle+ 21, screen.get_height() - 20 - 1-5])
-        self._blocks.append([middle - 21, screen.get_height() - 20 - 1-5])
 
     def draw(self):
         for block in self._blocks:
-            pygame.draw.rect(self._screen, (255,255, 255), pygame.Rect(block[0],block[1],20,20))
+            pygame.draw.rect(self._screen, (255,255, 255), pygame.Rect(block.x,block.y,20,20))
 
     def move(self,dx):
         #_blocks[0] - the middle block of plane
-        if self._blocks[0][0]+dx>22 and self._blocks[0][0] + dx < 14/22*self._screen.get_width()+20:
+        if self._blocks[0].x+dx>26 and self._blocks[0].x + dx < 14/22*self._screen.get_width()+13:
             for block in self._blocks:
-                block[0]+=dx
+                block.x+=dx
 
     def get_position(self):
-        return [self._blocks[1][0],self._blocks[1][1]]
+        return self._blocks[1]

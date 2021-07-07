8a751871db2df6a02d268a854854b3a62e0d2974
SzymonMs <84814415+SzymonMs@users.noreply.github.com>
2021-07-07 22:51:29 +0200

    Wersja 2.0

--- Szymon/Hunter/Game.py
+++ Szymon/Hunter/Game.py
@@ -11,7 +11,7 @@ class Game():
         screen.blit(text_wylacz, [screen.get_width() / 2 - 140, screen.get_height() / 2 + 10])
         screen.blit(text_wlacz, [screen.get_width() / 2 - 150, screen.get_height() / 2 + 30])
 
-    def sterowanie_oknem(self,player):
+    def sterowanie_oknem(self,player,shield):
         if pygame.key.get_pressed()[pygame.K_q]:
             pygame.quit()
             sys.exit()
@@ -20,6 +20,14 @@ class Game():
             player.punkty_ = 0
             player.x_ = 240
             player.y_ = 160
+            shield.dx_ = randrange(-6, 6, 1)
+            shield.dy_ = randrange(-5, 5, 1)
+            if shield.dx_ < 2 and shield.dx_ > -2:
+                shield.dx_ = 4
+            if shield.dy_ < 2 and shield.dy_ > -2:
+                shield.dy_ = -4
+
+
 
     def __init__(self):
         pygame.init()
@@ -34,6 +42,12 @@ class Game():
         screen = pygame.display.set_mode(size)
         player = Player(screen)
         shield = Shield(screen)
+        tlo= pygame.sprite.Sprite()
+
+        tlo.image = pygame.image.load("6.png").convert()
+
+        tlo.rect = tlo.image.get_rect()
+
 
         font_style = pygame.font.SysFont("dejavuserif", 20)
         font_style2 = pygame.font.SysFont("dejavuserif", 60)
@@ -48,34 +62,38 @@ class Game():
                         running = False
 
                 if player.zycia_ > 0:
-                    vx = 3 * randrange(-1, 1, 2)
-                    vy = 3 * randrange(-1, 1, 2)
 
-                    screen.fill((0, 0, 0))
+                    screen.blit(tlo.image, tlo.rect)
                     player.sterowanie(dx, dy)
 
                     shield.draw()
                     player.draw()
-                    shield.move(vx, vy, player)
+                    shield.move(player)
                     player.napisy(screen, font_style)
                     if shield.kolizja(player) == True:
-                        shield.x_ = randrange(20, 460, 1)
-                        shield.y_ = randrange(20, 300, 1)
+                        shield.x_ = randrange(180, 320, 1)
+                        shield.y_ = randrange(100, 280, 1)
+                        shield.dx_ = randrange(-6, 6, 1)
+                        shield.dy_ = randrange(-5, 5, 1)
+                        if shield.dx_<2 and shield.dx_>-2:
+                            shield.dx_=4
+                        if shield.dy_<2 and shield.dy_>-2:
+                            shield.dy_=-4
                         player.punkty_ += 1
                     if player.punkty_ >= 8:
-                        vx = 0
-                        vy = 0
+                        shield.dx_ = 0
+                        shield.dy_ = 0
                         screen.fill((0, 255, 127))
                         text_win = font_style2.render("Wygrana!", True, (0, 0, 0))
                         self.napisy_sterowanie(screen, font_style)
                         screen.blit(text_win, [screen.get_width() / 2 - 120, screen.get_height() / 2 - 50])
-                        self.sterowanie_oknem(player)
+                        self.sterowanie_oknem(player,shield)
                 if player.zycia_ <= 0:
                     screen.fill((255, 0, 0))
                     text_koniec = font_style2.render("Porażka!", True, (0, 0, 0))
                     screen.blit(text_koniec, [screen.get_width() / 2 - 120, screen.get_height() / 2 - 50])
                     self.napisy_sterowanie(screen, font_style)
-                    self.sterowanie_oknem(player)
+                    self.sterowanie_oknem(player,shield)
 
                 pygame.display.update()
                 pygame.display.flip()
--- Szymon/Hunter/Shield.py
+++ Szymon/Hunter/Shield.py
@@ -1,6 +1,7 @@
 import pygame
 from random import randrange
 import time
+
 class Shield(pygame.sprite.Sprite):
 
     def __init__(self,screen):
@@ -8,22 +9,26 @@ class Shield(pygame.sprite.Sprite):
         self.image_= pygame.image.load("shield.png").convert()
         self.screen_=screen
         self.rect_=self.image_.get_rect()
-        self.x_ =randrange(230, 250, 1)
-        self.y_ =randrange(150, 170, 1)
+        self.x_ =randrange(180, 320, 1)
+        self.y_ =randrange(100, 280, 1)
+        self.dx_=3 * randrange(-1, 1, 2)
+        self.dy_=3 * randrange(-1, 1, 2)
+
+
 
     def draw(self):
         self.screen_.blit(self.image_, self.rect_)
         self.rect_.center = [self.x_, self.y_]
 
-    def move(self, dx, dy,player):
-        if self.x_ + dx - 3 > -16 and self.x_ + dx + 3 < 496 and self.y_ + dy > -16 and self.y_ + dy < 336:
-            self.x_ += dx
-            self.y_ += dy
+    def move(self,player):
+        if self.x_ + self.dx_ - 3 > -16 and self.x_ + self.dx_ + 3 < 496 and self.y_ + self.dy_ > -16 and self.y_ + self.dy_ < 336:
+            self.x_ += self.dx_
+            self.y_ += self.dy_
         else:
             # self.x_ += dx
             # self.y_ += dy
-            self.x_ = randrange(20, 460, 1)
-            self.y_ = randrange(20, 300, 1)
+            self.x_ = randrange(180, 320, 1)
+            self.y_ =randrange(100, 280, 1)
             player.zycia_-=1
 
 
